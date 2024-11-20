386c2c3b8bf4d46fd39cf83cbb736c82516455e2
Jasr17 <salimivjasur@gmail.com>
2024-11-20 16:23:47 +0500

    zb

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      fontFamily:{
-        sans:["inter"]
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
